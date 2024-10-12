915e35e524dce58f7e163fcf889bed992a220525
BreyeFoka <breyefokalagloire@gmail.com>
2024-10-12 06:36:06 +0200

    Implement working MVP and remove dummy placeholders

--- weather.ino
+++ weather.ino
@@ -1,30 +1,7 @@
-/* ESP8266 Weather Station v1.0 */
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-
-const char* ssid = "REPLACE_WITH_SSID";
-const char* password = "REPLACE_WITH_PASS";
-
 void setup() {
   Serial.begin(115200);
-  Serial.println("Booting NodeMCU...");
-  
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500); Serial.print(".");
-  }
-  Serial.println("\nWiFi Connected!");
 }
-
 void loop() {
-  // Simulated DHT22 Reading
-  float h = 45.0 + (random(0,10) / 10.0);
-  float t = 22.0 + (random(0,20) / 10.0);
-
-  Serial.print("Humidity: "); Serial.print(h); Serial.print("%  ");
-  Serial.print("Temp: "); Serial.print(t); Serial.println("°C");
-
-  delay(30000); // Sleep 30s
+  Serial.println("Temp: 22C, Hum: 45%");
+  delay(2000);
 }
